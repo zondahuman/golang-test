cb17f819a86f94d81bcdfc1d8f769dfa3670a13e
zondahuman <zondahuman@gmail.com>
2017-06-05 16:41:17 +0800

    add

--- com/mq/rabbitmq/test/RabbitmqUtil_test.go
+++ com/mq/rabbitmq/test/RabbitmqUtil_test.go
@@ -5,15 +5,44 @@ import (
 	"golang-test/com/mq/rabbitmq/service"
 	"fmt"
 	"time"
+	"runtime"
+	"golang-test/com/mq/rabbitmq/constants"
 )
 
 func Test_receive(t *testing.T) {
 
 	go func() {
 		for {
-			service.Push("TL")
+			go service.Push("TL")
 			time.Sleep(1 * time.Second)
 		}
+	}()
+	service.Receive()
+	fmt.Println("end")
+	service.Close()
+}
+
+func Test_Call(t *testing.T) {
+
+	go func() {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		for {
+			var limit = constants.HUNDRED
+			channel := make(chan int, limit)
+			for i := 0; i < limit; i++ {
+				go func() {
+					channel <- service.Push("TL")
+				}()
+				fmt.Println(<-channel)
+			}
+
+			close(channel)
+		}
+
+		//for {
+		//	go service.Push("TL")
+		//	time.Sleep(1 * time.Second)
+		//}
 	}()
 	service.Receive()
 	fmt.Println("end")
